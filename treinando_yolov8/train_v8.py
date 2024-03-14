346d35196e3bbb3bf3c9e99e1f2bd3526ce67ae7
william cardoso <williancard123@gmail.com>
2024-03-14 08:57:28 -0400

    train with new database

--- treinando_yolov8/train_v8.py
+++ treinando_yolov8/train_v8.py
@@ -1,15 +1,16 @@
 from ultralytics import YOLO
-
+import os
+os.environ['CUDA_VISIBLE_DEVICES'] = '0'
 # para marcar as imagens
 # https://www.makesense.ai/
 
 def main():
     # Load a model
     #model = YOLO("yolov8n.yaml")  # build a new model from scratch
-    model = YOLO("yolov8x-cls.pt")  # load a pretrained model (recommended for training)
+    model = YOLO("yolov8m-cls.pt")  # load a pretrained model (recommended for training)
 
     # Use the model
-    model.train(data="../datasets", epochs=50, device=0, imgsz=64)  # train the model
+    model.train(data="../datasets", epochs=50, device=0, imgsz=64, batch=64)  # train the model
     metrics = model.val()  # evaluate model performance on the validation set
     # results = model("https://ultralytics.com/images/bus.jpg")  # predict on an image
     # path = model.export(format="onnx")  # export the model to ONNX format
